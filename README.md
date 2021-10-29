26302eefd48ca32079e764215fb6bca7f4827ee8
shawnelldh <shawnelldh@gmail.com>
2021-10-28 21:17:30 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-iov3a8
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-iov3a8)
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./style.css";
+import Header from "./components/Header";
+import Body from "./components/Body";
+
+export default function App() {
+  return (
+    <div className="App">
+     <Header/>
+     <Header/>
+     <Body/>
+    </div>
+  );
+}
+
+export default App;
