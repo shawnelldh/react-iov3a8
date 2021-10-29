1f7eed2e04e2d71c97cf2f871616d8d1994f1d65
shawnelldh <shawnelldh@gmail.com>
2021-10-28 21:22:55 -0400

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./style.css";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
      <Header/>
      <Header/>
      <Body/>
+     <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+
+const Footer = () => {
+   return (
+      <div>
+   <h1> Footer</h1>
+
+      </div>
+     
+
+
+   )
+
+
+}
+
+export default Footer;
